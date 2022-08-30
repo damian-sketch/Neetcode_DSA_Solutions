f055b1d547396ea6ce1227cd5f299bbf13147de8
damian-sketch <cedricdamian19@gmail.com>
2022-08-30 22:32:39 +0300

    Solved preorder Traversal with recursive algorithm

---
+++ Python_DSA/preorderTraversal.py
@@ -0,0 +1,22 @@
+# PROBLEM STATEMENT
+#  Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+# SOLUTION
+#  Write a recursive function with the base condition checking if there is a root. The recursive function should return
+# a list with the root value and append to it with recursion on the left values then the right values
+from typing import Optional, List
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
